b355a0951e1eb3649ed8d7bd2b81f550b4db7379
Alli Tzadok <allisontzadok@gmail.com>
2018-01-23 18:04:31 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,16 +1,23 @@
 class MP3Importer
-  #Parses through a directory of files
-  #sends the filenames to a song class to create a music library
-  #the library should be made up of music with artists that are unique
+
+
+
 
   def initialization(path)
     @path = path
   end
 
+  #Parses through a directory of files
+  #collects filenames and saves just the filename, not ext
+
   def files
     @files ||= Dir.glob("#{path}/*.mp3").collect{ |filename|filename.gsub("#{path}/", "")}
   end
 
+  #sends the filenames to a song class to create a music library
+
+  #the library should be made up of music with artists that are unique
+  # # # this is probably done on the artist class.
   def import
 
     file.each {|filename| Song.new_by_filename(filename)}
