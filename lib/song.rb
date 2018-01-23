91827ecfd89fffd064a0196db4075ea2328de76e
Alli Tzadok <allisontzadok@gmail.com>
2018-01-23 19:09:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,20 +9,23 @@ class Song
     @name = name
   end
 
-  #create a new song instance using the string we gather from filename
+  
   def self.new_by_filename(filename)
     file_details = filename.split(" - ")
     artist = file_details[0]
     song = file_details[1]
+    #create a new song instance using the string we gather from filename
     new_song = self.new(song)
     new_song
-
   end
 
 
   #associate the new song with an artist.
   # # # note - use helper method: artist_name=(artist_name)
   #this method should takes in the name and gets the artist object
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song
 
   #return the new song instance
 
