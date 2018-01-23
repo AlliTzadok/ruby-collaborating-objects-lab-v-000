3e04392a1b6339563fbfd501325744d58c831586
Alli Tzadok <allisontzadok@gmail.com>
2018-01-23 18:59:52 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -17,7 +17,7 @@ class MP3Importer
   #the library should be made up of music with artists that are unique
   # # # this is probably done on the artist class.
   def import
-    file.each {|filename| Song.new_by_filename(filename)}
+    files.each {|filename| Song.new_by_filename(filename)}
   end
 
 end
