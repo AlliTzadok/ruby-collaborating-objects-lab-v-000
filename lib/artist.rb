30a9f6bb5ff0b62fd2f38c1d0ce873c11c3d2814
Alli Tzadok <allisontzadok@gmail.com>
2018-01-23 18:05:26 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,8 @@ class Artist
 
   #the library should be made up of music with artists that are unique
   # # # this is probably done on the artist class.
-
+  def initialize(name)
+    @name = name
+  end
 
 end
