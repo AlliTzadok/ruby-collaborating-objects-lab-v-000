d7cf3d3e9177dda67d49319ef2c9b47cb051efa5
Alli Tzadok <allisontzadok@gmail.com>
2018-01-23 17:41:20 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,3 +1,18 @@
 class MP3Importer
+  #Parses through a directory of files
+  #sends the filenames to a song class to create a music library
+  #the library should be made up of music with artists that are unique
+
+  def initialization(path)
+    @path = path
+  end
+
+  def files
+  end
+
+  def import
+  end
+
+  
 
 end
