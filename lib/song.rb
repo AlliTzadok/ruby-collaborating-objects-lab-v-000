c2e41a1bc5319f0548d6b5238e98e3ce7e5b3bea
Alli Tzadok <allisontzadok@gmail.com>
2018-01-23 17:49:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,16 @@
 class Song
 
+  #parse a filename to find the song name and artist name.
+  # # # note - every filename is separated by " - "
+
+
+  #create a new song instance using the string we gather from filename
+  
+
+  #associate the new song with an artist. 
+  # # # note - use helper method: artist_name=(artist_name)
+  #this method should takes in the name and gets the artist object
+
+  #return the new song instance
+  
 end
